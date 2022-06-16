fe02521420757659e624e5ed6fdcecf0c83836bd
asfafasa <giftofgodashish@gmail.com>
2022-06-16 14:54:24 +0530

    Added heroku and UI

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://127.0.0.1:27017/simba_app" ||
+    "mongodb+srv://ashish:vm3hrZS8T0owJBGx@mernbasic.w0ajb.gcp.mongodb.net/purchase_app?retryWrites=true&w=majority",
+};
